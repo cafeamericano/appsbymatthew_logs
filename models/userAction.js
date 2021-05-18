aee763da6f81da341addf9543a11dd7353b4439f
Matthew Farmer <matthewfarmer@matthews-air.lan>
2021-05-17 21:13:05 -0400

    Able to capture ip address and browser type

--- models/userAction.js
+++ models/userAction.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userActionSchema = new Schema({
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+    application: String,
+    sublocation: String,
     description: String,
-    operation: String
+    operation: String,
+    ip_address: String,
+    browser: String
 });
 
 module.exports = mongoose.model('UserAction', userActionSchema);
